c771a8262e60f60bd69d923aa7a5e88b87084047
Alexander Minza <alexander.minza@gmail.com>
2025-04-17 13:11:07 +0300

    tasks Python: clean

--- src/maib_ecommerce_sdk/__init__.py
+++ src/maib_ecommerce_sdk/__init__.py
@@ -1,5 +1,5 @@
 """Python SDK for maib ecommerce API"""
 
-from .maibsdk import MaibSdk, MaibTokenException, MaibPaymentException
-from .maibauth import MaibAuthRequest, MaibAuth
-from .maibapi import MaibApiRequest, MaibApi
+from .maib_sdk import MaibSdk, MaibTokenException, MaibPaymentException
+from .maib_auth import MaibAuthRequest, MaibAuth
+from .maib_api import MaibApiRequest, MaibApi
