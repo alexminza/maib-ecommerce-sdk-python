4f8fea3d5b2b867e9a09f0c457a0b63d3f7c802d
Alexander Minza <alexander.minza@gmail.com>
2025-08-29 16:54:24 +0300

    handle_response

--- src/maib_ecommerce_sdk/maib_sdk.py
+++ src/maib_ecommerce_sdk/maib_sdk.py
@@ -77,8 +77,10 @@ class MaibSdk:
     def handle_response(response: dict, endpoint: str):
         """Handles errors returned by the API."""
 
-        response_ok = response.get('ok')
-        if response_ok is not None and response_ok is True:
+        if not isinstance(response, dict):
+            raise MaibPaymentException(f"Invalid response received from server for endpoint {endpoint}")
+
+        if response.get('ok') is True:
             response_result: dict = response.get('result')
             if response_result is not None:
                 return response_result
@@ -86,8 +88,8 @@ class MaibSdk:
             raise MaibPaymentException(f'Invalid response received from server for endpoint {endpoint}: missing \'result\' field.')
 
         response_errors = response.get('errors')
-        if response_errors is not None:
-            error = response_errors[0]
+        if isinstance(response_errors, list) and response_errors:
+            error: dict = response_errors[0]
             raise MaibPaymentException(f'Error sending request to endpoint {endpoint}: {error.get('errorMessage')} ({error.get('errorCode')})')
 
         raise MaibPaymentException(f'Invalid response received from server for endpoint {endpoint}: missing \'ok\' and \'errors\' fields')
